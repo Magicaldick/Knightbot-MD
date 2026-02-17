e818efe3d05481cc6bd03512032a7925aee4f459
Magicaldick <142235219+Magicaldick@users.noreply.github.com>
2026-02-17 23:27:16 +0300

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,14 +1,14 @@
 const settings = {
-  packname: 'Knight Bot',
+  packname: 'The Knight',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '919876543210', //Set your number here without + symbol, just add country code & number without any space
+  botName: "The Knight",
+  botOwner: 'Salim', // Your name
+  ownerNumber: '254746314005', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   maxStoreMessages: 20, 
   storeWriteInterval: 10000,
-  description: "This is a bot for managing group commands and automating tasks.",
+  description: "This is a bot for managing group commands,automating tasks and making Salim's life easier 😎",
   version: "3.0.6",
   updateZipUrl: "https://github.com/mruniquehacker/Knightbot-MD/archive/refs/heads/main.zip",
 };
